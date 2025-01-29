6a9d72a2f18ffc4177465ef3dc7c47ffc555373f
surajshukla <surajshukla5604@gmail.com>
2025-01-29 08:06:13 +0530

    feat: Setup Project Structure

---
+++ build/CMakeFiles/Crush.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Crush.dir/src/main.c.o"
+  "CMakeFiles/Crush.dir/src/main.c.o.d"
+  "Crush"
+  "Crush.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Crush.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
